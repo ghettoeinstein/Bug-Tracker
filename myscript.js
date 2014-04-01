38b3d7e9acce4b214bb74df9f05f140e1df94598
Caleb Saunders <csaunders1@toromail.csudh.edu>
2014-04-01 13:33:37 -0700

    Added saving of bugs

--- myscript.js
+++ myscript.js
@@ -1,29 +1,34 @@
-var bugs = new Array();
-
-
-
 window.onload = function() {
-    if (sessionStorage.name == "") {   
-           sessionStorage.name = ("What is your name noble Bug Tester?");
-        }
-    else{
-      //  alert("Hello " + sessionStorage.getItem("name"));
-     }
-   
+  
+  //Check if there is an existing bugObject. If not create one.
+
+  
+  bugObject = {};
+  bugObject.completedBugs = [];
+  bugObject.incompleteBugs = [];
+  json = JSON.stringify(bugObject);
+  localStorage.setItem("bugObject", json);
+
+ 
  }
  
  
  function addBug(){
     var bug = document.getElementById('bug').value;
-        if (isNan(bug)) {
-            alert("Nice try funny guy, enter a real number");
-            return;
-         }
-    
+     
     var node = document.createElement("li");
+    
     var textnode = document.createTextNode(bug);
     node.appendChild(textnode);
     document.getElementById('bugList').appendChild(node);
+    
+    var retrievedJson = localStorage.getItem("bugObject");
+    bugObject = JSON.parse(retrievedJson);
+    bugObject.completedBugs.push(bug);
+    
+    json = JSON.stringify(bugObject);
+    localStorage.setItem("bugObject", json);
+    
     
     }
  
